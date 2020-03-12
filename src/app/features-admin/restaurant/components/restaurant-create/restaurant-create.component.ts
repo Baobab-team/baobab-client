4ddb5911eb2bb8d67d9e0f6bf19a683b323c2f4a
willy <willkoua@gmail.com>
2020-03-12 17:36:05 +0100

    fix restaurant create admin page

--- src/app/features-admin/restaurant/components/restaurant-create/restaurant-create.component.ts
+++ src/app/features-admin/restaurant/components/restaurant-create/restaurant-create.component.ts
@@ -1,7 +1,9 @@
+import { TranslateService } from '@ngx-translate/core';
 import { BUSINESS_PAYMENT_TYPES, BUSINESS_LANGUAGE } from './../../../../core/models/business.model';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { RxFormBuilder } from '@rxweb/reactive-form-validators';
+import { RxFormBuilder, RxwebValidators } from '@rxweb/reactive-form-validators';
+
 
 @Component({
   selector: 'app-restaurant-create',
@@ -33,9 +35,22 @@ export class RestaurantCreateComponent implements OnInit {
   get f() { return this.restaurantForm.controls; }
 
   ngOnInit() {
-    // this.restaurantForm = this.formBuilder.formGroup(
-    //   new Restaurant()
-    // );
+    this.restaurantForm = this.formBuilder.group(
+      {
+        name: ['', [RxwebValidators.required({message: 'admin.business.message_errors.name_required'})]],
+        description: [''],
+        note: [''],
+        website: ['', [RxwebValidators.url({message: 'admin.business.message_errors.url_valid'})]],
+        email: ['', [
+          RxwebValidators.required({message: 'admin.business.message_errors.email_required'}),
+          RxwebValidators.email({message: 'admin.business.message_errors.email_valid'})
+        ]],
+        slogan: [''],
+        language: ['', [RxwebValidators.required({message: 'admin.business.message_errors.language_required'})]],
+        capacity: ['', [RxwebValidators.numeric({message: 'admin.business.message_errors.numeric_valid'})]],
+        paymentType: [''],
+      }
+    );
   }
 
   onSubmit() {
