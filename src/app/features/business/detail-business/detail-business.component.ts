bee575d39a7ac5672af8db8a53e701e787397b27
willy <willkoua@gmail.com>
2020-03-23 11:31:24 +0100

    init detail business

---
+++ src/app/features/business/detail-business/detail-business.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-business',
+  templateUrl: './detail-business.component.html',
+  styleUrls: ['./detail-business.component.scss']
+})
+export class DetailBusinessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
