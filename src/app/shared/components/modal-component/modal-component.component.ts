7c477d3ad0909e80f08725a8a5483a425bb63d57
willy <willkoua@gmail.com>
2020-05-09 13:58:49 +0100

    add modal component

---
+++ src/app/shared/components/modal-component/modal-component.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Modal } from 'src/app/core/models/modal.model';
+
+
+@Component({
+  selector: 'app-modal-component',
+  templateUrl: './modal-component.component.html',
+  styleUrls: ['./modal-component.component.scss']
+})
+export class ModalComponentComponent implements OnInit {
+  @Output() confirmEvent: EventEmitter<any> = new EventEmitter();
+  @Input() modal: Modal;
+
+  closeResult: string;
+
+  constructor(public activeModal: NgbActiveModal) { }
+
+  ngOnInit(): void {
+  }
+
+  confirm() {
+    // this.confirmEvent.emit(this.modal);
+    this.activeModal.close(this.modal);
+  }
+}
