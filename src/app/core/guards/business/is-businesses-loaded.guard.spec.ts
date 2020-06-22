2e9afdbe7e1b16fd64c5b4a09080e7c7af668662
willy <willkoua@gmail.com>
2020-06-22 21:46:06 +0100

    fix guard

--- src/app/core/guards/business/is-businesses-loaded.guard.spec.ts
+++ src/app/core/guards/business/is-businesses-loaded.guard.spec.ts
@@ -1,11 +1,34 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { IsBusinessesLoadedGuard } from './is-businesses-loaded.guard';
+import { StoreModule, Store } from '@ngrx/store';
+import { REDUCER_TOKEN, getReducers } from '@Store/index';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IsCategoriesLoadedGuard } from '@Guards/is-categories-loaded.guard';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApiPrefixInterceptor } from '@Interceptors/api-prefix.interceptor';
 
 describe('IsBusinessesLoadedGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [IsBusinessesLoadedGuard]
+      imports: [
+        StoreModule.forRoot(REDUCER_TOKEN),
+        RouterTestingModule
+      ],
+      providers: [
+        IsCategoriesLoadedGuard,
+        Store,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: ApiPrefixInterceptor,
+          multi: true
+        },
+        {
+          provide: REDUCER_TOKEN,
+          useFactory: getReducers
+        },
+        IsBusinessesLoadedGuard,
+      ],
     });
   });
 
