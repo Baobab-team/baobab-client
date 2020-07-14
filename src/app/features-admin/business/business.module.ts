d882d48e033bc50083cadf63b8376b39f9d74229
willy kouagnia <willkoua@gmail.com>
2020-07-14 14:09:34 +0100

    change datatable in business list component

--- src/app/features-admin/business/business.module.ts
+++ src/app/features-admin/business/business.module.ts
@@ -1,13 +1,12 @@
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '../../shared';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BusinessRoutingModule } from './business-routing.module';
 import { BusinessCreateComponent } from './components/business-create/business-create.component';
 import { BusinessListComponent } from './components/business-list/business-list.component';
 import { BusinessDetailsComponent } from './components/business-details/business-details.component';
 import { UploadFileComponent } from './components/upload-file/upload-file.component';
-
+import { AgGridModule} from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
@@ -20,6 +19,7 @@ import { UploadFileComponent } from './components/upload-file/upload-file.compon
     CommonModule,
     SharedModule,
     BusinessRoutingModule,
+    AgGridModule.withComponents([]),
   ]
 })
 export class BusinessModule { }
