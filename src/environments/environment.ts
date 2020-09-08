aa14a5f588f35a1a7bcc10c136399a1917541f24
Sylvain Dégué <sylvain.degue@gmail.com>
2020-09-07 20:50:02 -0400

    put back env vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,11 @@
 export const environment = {
   production: false,
   baseApiUrl: 'http://localhost:5000',
+  apiVersion: 'api_v1',
+  paths_api: {
+    businesses: 'businesses',
+    categories: 'categories',
+  },
+  defaultLanguage: 'fr',
+  supportedLanguages: ['fr', 'en'],
 };
